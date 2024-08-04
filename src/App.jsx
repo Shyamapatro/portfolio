3f123aafd7414e2cf42e8638f3de4492d3f2cb99
Shyama patro <shyama.patro@gmail.com>
2024-08-04 20:39:06 +0530

    add-loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,37 @@
+// src/App.js
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Contact from './sections/Contact/Contact';
 import Footer from './sections/Footer/Footer';
 import Hero from './sections/Hero/Hero';
 // import Projects from './sections/Projects/Projects';
 import Skills from './sections/Skills/Skills';
-
+import Loader from './sections/Loader/ModernLoader';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a network request
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
-      <Hero />
-      {/* <Projects /> */}
-      <Skills />
-      <Contact />
-      <Footer />
+      {loading && <Loader />}
+      {!loading && (
+        <>
+          <Hero />
+          {/* <Projects /> */}
+          <Skills />
+          <Contact />
+          <Footer />
+        </>
+      )}
     </>
   );
 }
---
+++ src/sections/Loader/ModernLoader.jsx
@@ -0,0 +1,16 @@
+// src/components/ModernLoader/ModernLoader.js
+import React from 'react';
+import './ModernLoader.css';
+
+const ModernLoader = () => {
+  return (
+    <div className="modern-loader">
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+    </div>
+  );
+};
+
+export default ModernLoader;
